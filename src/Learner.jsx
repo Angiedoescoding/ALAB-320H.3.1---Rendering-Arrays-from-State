bb77cc1b3a093a1043184a5978f091d13905afd7
Angelica <139031564+Angiedoescoding@users.noreply.github.com>
2024-03-04 21:15:06 -1000

    adding Scores component

--- src/Learner.jsx
+++ src/Learner.jsx
@@ -1,15 +1,15 @@
 import Score from "./Score";
 
 export default function Learner () {
-    const scores = [];
+    const scores = [];              // Initializing an empty arraye of scores
 
-    learners.scores.forEach((score, index) => {
+    learners.scores.forEach((score, index) => {         // Itterating thoruth the each score of the learners via forEach
         scores.push(
-            <Score key={index} date={score.date} score={score.score} />
+            <Score key={index} date={score.date} score={score.score} />         // Creating a Score for each score and assignint it a unique key prop and passing the date and score props
         );
     });
 
-    return (
+    return (                // Rendering the learner's info: name, bia nad the scores (the list of all scores)
         <div>
             <h2>{learner.name}</h2>
             <p>{learner.bio}</p>
---
+++ src/Score.jsx
@@ -0,0 +1,10 @@
+export default function Score () {
+    const Score = {date, score}         // Setting props - date and score
+    return (
+         // Displaying the date abd score
+        <div> 
+        <p>Date: { data }</p>;         
+        <p>Score: { score }</p>;        
+        </div>
+    );
+}
